26cd334277613bfff24018da3b4ec03da9476e7d
hikkathon <hikkathon@gmail.com>
2020-04-12 19:05:08 +0700

    up

--- SWS/SWSv2/EntryModel.cs
+++ SWS/SWSv2/EntryModel.cs
@@ -12,20 +12,20 @@ namespace SWSv2
         public string Title             { get; set; } // Название в заголовке 
         public string Rating            { get; set; } // Рейтинг
         public string Vote              { get; set; } // Сколько пользователей проголосовало
-        public string titleList         { get; set; } // Альтернативные название
+        public List<string> titleList   { get; set; } // Альтернативные название
         public string View              { get; set; } // Колличество просмотров
         public string Status            { get; set; } // Статус (Онгоин, вышел и тд.)
         public string Released          { get; set; } // Дата выхода
         public string Season            { get; set; } // Сезон выхода
         public string ageRating         { get; set; } // Возростной рейтинг
-        public string Genre             { get; set; } // Жанр
+        public List<string> Genre       { get; set; } // Жанр
         public string primarySource     { get; set; } // Первоисточник
         public string Studio            { get; set; } // Студия 
         public string Producer          { get; set; } // Режиссер
         public string Type              { get; set; } // Тип (Сериал, полнометражный, ОВА и тд.)
         public string Series            { get; set; } // Количество серий
-        public string Transfer          { get; set; } // Перевод (Многоголосный, одноголосный или субтитры)
-        public string voiceActing       { get; set; } // Проект который озвучил (AniDub, Anilibria и тд.)
+        public List<string> Transfer    { get; set; } // Перевод (Многоголосный, одноголосный или субтитры)
+        public List<string> voiceActing { get; set; } // Проект который озвучил (AniDub, Anilibria и тд.)
         public string Description       { get; set; } // Описание
         public string urlImage          { get; set; } // Постер
         public string License           { get; set; } // Издатель
--- SWS/SWSv2/Scraper.cs
+++ SWS/SWSv2/Scraper.cs
@@ -34,62 +34,73 @@ namespace SWSv2
                 int counter = 0;
                 try
                 {
-                    var posts = htmlDoc.DocumentNode.SelectNodes(".//div[@class='content-page categories-page']/div[@class='anime-column']");
+                    var posts = htmlDoc.DocumentNode.SelectNodes(".//div[@class='content-page categories-page']/div[@class='anime-column']"); // в переменную post парсим аниме из каталога
 
+                    // Циклом проходимся по всем аниме
                     foreach (var post in posts)
                     {
                         counter++;
 
-                        var hrefitem = post.SelectSingleNode("./a[@class='image-block']")?.GetAttributes("href");
+                        var hrefitem = post.SelectSingleNode("./a[@class='image-block']")?.GetAttributes("href"); // вытягиваем ссылки на страницу с аниме
 
                         var web = new HtmlWeb();
                         var htmlitem = web.Load("https://yummyanime.club" + hrefitem.ElementAt(0).Value);
 
-                        var title = htmlitem.DocumentNode.SelectSingleNode("/html/body/div[3]/div[3]/div/div/h1")?.InnerText.Trim();
-                        var rating = htmlitem.DocumentNode.SelectSingleNode("/html/body/div[3]/div[3]/div/div/div[2]/span[2]/span[1]")?.InnerText.Trim() ?? "Рейтинг недоступен";
-                        var vote = htmlitem.DocumentNode.SelectSingleNode("/html/body/div[3]/div[3]/div/div/div[2]/span[2]/span[2]")?.InnerText.Trim() ?? "Рейтинг недоступен";
-                        var titleList = htmlitem.DocumentNode.SelectSingleNode("/html/body/div[3]/div[3]/div/div/ul[1]")?.InnerText.Trim();
-                        var view = htmlitem.DocumentNode.SelectSingleNode("/html/body/div[3]/div[3]/div/div/ul[2]/li[1]/text()")?.InnerText.Trim().Replace(" ","");
-                        var status = htmlitem.DocumentNode.SelectSingleNode("/html/body/div[3]/div[3]/div/div/ul[2]/li[2]/span[2]")?.InnerText.Trim();
+                        #region вытягиваем инфу об анимках
+                        var title = htmlitem.DocumentNode.SelectSingleNode("//h1")?.InnerText.Trim();
+                        var rating = htmlitem.DocumentNode.SelectSingleNode("//span[@class='main-rating']")?.InnerText.Trim() ?? "Рейтинг недоступен";
+                        var vote = htmlitem.DocumentNode.SelectSingleNode("//span[@class='main-rating-info']")?.InnerText.Trim() ?? "Рейтинг недоступен";
+
+                        var titleList = htmlitem.DocumentNode.SelectNodes(".//ul[@class='alt-names-list']/li");
+
+                        var view = htmlitem.DocumentNode.SelectSingleNode(".//ul[@class='content-main-info']/li[1]/text()")?.InnerText.Trim().Replace(" ","") ?? "Нет просмотров";
+                        var status = htmlitem.DocumentNode.SelectSingleNode(".//html/body/div[3]/div[3]/div/div/ul[2]/li[2]/span[2]")?.InnerText.Trim();
                         var released = htmlitem.DocumentNode.SelectSingleNode("/html/body/div[3]/div[3]/div/div/ul[2]/li[3]/text()")?.InnerText.Trim();
                         var season = htmlitem.DocumentNode.SelectSingleNode("/html/body/div[3]/div[3]/div/div/ul[2]/li[4]/text()")?.InnerText.Trim();
-                        var ageRating = htmlitem.DocumentNode.SelectSingleNode("/html/body/div[3]/div[3]/div/div/ul[2]/li[5]/text()")?.InnerText.Trim();
-                        var genre = htmlitem.DocumentNode.SelectSingleNode("/html/body/div[3]/div[3]/div/div/ul[2]/li[6]/ul")?.InnerText.Trim();
+                        var ageRating = htmlitem.DocumentNode.SelectSingleNode("/html/body/div[3]/div[3]/div/div/ul[2]/li[5]/text()")?.InnerText.Trim() ?? "Не установлен";
+
+                        var genre = htmlitem.DocumentNode.SelectNodes(".//ul[@class='categories-list']/li");
+
                         var primarySourse = htmlitem.DocumentNode.SelectSingleNode("/html/body/div[3]/div[3]/div/div/ul[2]/li[7]/text()")?.InnerText.Trim();
                         var studio = htmlitem.DocumentNode.SelectSingleNode("/html/body/div[3]/div[3]/div/div/ul[2]/li[8]/ul/li/a")?.InnerText.Trim();
                         var producer = htmlitem.DocumentNode.SelectSingleNode("/html/body/div[3]/div[3]/div/div/ul[2]/li[9]/a")?.InnerText.Trim();
                         var type = htmlitem.DocumentNode.SelectSingleNode("//*[@id='animeType']/text()")?.InnerText.Trim();
                         var series = htmlitem.DocumentNode.SelectSingleNode("/html/body/div[3]/div[3]/div/div/ul[2]/li[11]/text()")?.InnerText.Trim();
+
                         var transfer = htmlitem.DocumentNode.SelectSingleNode("/html/body/div[3]/div[3]/div/div/ul[2]/li[12]/ul")?.InnerText.Trim();
+
                         var voiceActing = htmlitem.DocumentNode.SelectSingleNode("/html/body/div[3]/div[3]/div/div/ul[2]/li[13]/ul")?.InnerText.Trim();
+
                         var description = htmlitem.DocumentNode.SelectSingleNode("//*[@id='content - desc - text']/p")?.InnerText.Trim();
+
                         var urlImage = htmlitem.DocumentNode.SelectSingleNode("/html/body/div[3]/div[3]/div/div/div[1]/div[1]/img")?.InnerText.Trim();
                         var licende = htmlitem.DocumentNode.SelectSingleNode("//*[@id='video']/div")?.InnerText.Trim();
+                        #endregion
 
-                        _entries.Add(new EntryModel
-                        {
-                            Number = counter,
-                            Title = title,
-                            Rating = rating,
-                            Vote = vote,
-                            titleList = titleList,
-                            View = view,
-                            Status = status,
-                            Released = released,
-                            Season = season,
-                            ageRating = ageRating,
-                            Genre = genre,
-                            primarySource = primarySourse,
-                            Studio = studio,
-                            Producer = producer,
-                            Type = type,
-                            Series = series,
-                            Transfer = transfer,
-                            voiceActing = voiceActing,
-                            Description = description,
-                            urlImage = "https://yummyanime.club" + urlImage,
-                            License = licende
-                        });
+                        //_entries.Add(new EntryModel
+                        //{
+                        //    Number = counter,
+                        //    Title = title,
+                        //    Rating = rating,
+                        //    Vote = vote,
+                        //    titleList = titleList,
+                        //    View = view,
+                        //    Status = status,
+                        //    Released = released,
+                        //    Season = season,
+                        //    ageRating = ageRating,
+                        //    Genre = genre,
+                        //    primarySource = primarySourse,
+                        //    Studio = studio,
+                        //    Producer = producer,
+                        //    Type = type,
+                        //    Series = series,
+                        //    Transfer = transfer,
+                        //    voiceActing = voiceActing,
+                        //    Description = description,
+                        //    urlImage = "https://yummyanime.club" + urlImage,
+                        //    License = licende
+                        //});
                     }
                 }
                 catch (NullReferenceException exc)
