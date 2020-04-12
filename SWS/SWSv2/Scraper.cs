e1c1d8c65611dd45d715f5af059e0698fd4ad08b
hikkathon <hikkathon@gmail.com>
2020-04-12 20:45:59 +0700

    up

--- SWS/SWSv2/Scraper.cs
+++ SWS/SWSv2/Scraper.cs
@@ -36,71 +36,74 @@ namespace SWSv2
                 {
                     var posts = htmlDoc.DocumentNode.SelectNodes(".//div[@class='content-page categories-page']/div[@class='anime-column']"); // в переменную post парсим аниме из каталога
 
-                    // Циклом проходимся по всем аниме
-                    foreach (var post in posts)
+                    for (int i = 0; i < htmlDoc.DocumentNode.SelectNodes(".//div[@class='content-page categories-page']/div[@class='anime-column']").Count(); i++)
                     {
-                        counter++;
+                        // Циклом проходимся по всем аниме
+                        foreach (var post in posts)
+                        {
+                            counter++;
 
-                        var hrefitem = post.SelectSingleNode("./a[@class='image-block']")?.GetAttributes("href"); // вытягиваем ссылки на страницу с аниме
+                            var hrefitem = post.SelectSingleNode("./a[@class='image-block']")?.GetAttributes("href"); // вытягиваем ссылки на страницу с аниме
 
-                        var web = new HtmlWeb();
-                        var htmlitem = web.Load("https://yummyanime.club" + hrefitem.ElementAt(0).Value);
+                            var web = new HtmlWeb();
+                            var htmlitem = web.Load("https://yummyanime.club" + hrefitem.ElementAt(0).Value);
 
-                        #region вытягиваем инфу об анимках
-                        var title = htmlitem.DocumentNode.SelectSingleNode("//h1")?.InnerText.Trim();
-                        var rating = htmlitem.DocumentNode.SelectSingleNode("//span[@class='main-rating']")?.InnerText.Trim() ?? "Рейтинг недоступен";
-                        var vote = htmlitem.DocumentNode.SelectSingleNode("//span[@class='main-rating-info']")?.InnerText.Trim() ?? "Рейтинг недоступен";
+                            #region вытягиваем инфу об анимках
+                            var title = htmlitem.DocumentNode.SelectSingleNode("//h1")?.InnerText.Trim();
+                            var rating = htmlitem.DocumentNode.SelectSingleNode("//span[@class='main-rating']")?.InnerText.Trim() ?? "Рейтинг недоступен";
+                            var vote = htmlitem.DocumentNode.SelectSingleNode("//span[@class='main-rating-info']")?.InnerText.Trim() ?? "Рейтинг недоступен";
 
-                        var titleList = htmlitem.DocumentNode.SelectNodes(".//ul[@class='alt-names-list']/li");
+                            var titleList = htmlitem.DocumentNode.SelectNodes(".//ul[@class='alt-names-list']/li");
 
-                        var view = htmlitem.DocumentNode.SelectSingleNode(".//ul[@class='content-main-info']/li[1]/text()")?.InnerText.Trim().Replace(" ","") ?? "Нет просмотров";
-                        var status = htmlitem.DocumentNode.SelectSingleNode(".//html/body/div[3]/div[3]/div/div/ul[2]/li[2]/span[2]")?.InnerText.Trim();
-                        var released = htmlitem.DocumentNode.SelectSingleNode("/html/body/div[3]/div[3]/div/div/ul[2]/li[3]/text()")?.InnerText.Trim();
-                        var season = htmlitem.DocumentNode.SelectSingleNode("/html/body/div[3]/div[3]/div/div/ul[2]/li[4]/text()")?.InnerText.Trim();
-                        var ageRating = htmlitem.DocumentNode.SelectSingleNode("/html/body/div[3]/div[3]/div/div/ul[2]/li[5]/text()")?.InnerText.Trim() ?? "Не установлен";
+                            var view = htmlitem.DocumentNode.SelectSingleNode(".//ul[@class='content-main-info']/li[1]/text()")?.InnerText.Trim().Replace(" ", "") ?? "Нет просмотров";
+                            var status = htmlitem.DocumentNode.SelectSingleNode(".//html/body/div[3]/div[3]/div/div/ul[2]/li[2]/span[2]")?.InnerText.Trim();
+                            var released = htmlitem.DocumentNode.SelectSingleNode("/html/body/div[3]/div[3]/div/div/ul[2]/li[3]/text()")?.InnerText.Trim();
+                            var season = htmlitem.DocumentNode.SelectSingleNode("/html/body/div[3]/div[3]/div/div/ul[2]/li[4]/text()")?.InnerText.Trim();
+                            var ageRating = htmlitem.DocumentNode.SelectSingleNode("/html/body/div[3]/div[3]/div/div/ul[2]/li[5]/text()")?.InnerText.Trim() ?? "Не установлен";
 
-                        var genre = htmlitem.DocumentNode.SelectNodes(".//ul[@class='categories-list']/li");
+                            var genre = htmlitem.DocumentNode.SelectNodes(".//ul[@class='categories-list']/li[6]/ul");
 
-                        var primarySourse = htmlitem.DocumentNode.SelectSingleNode("/html/body/div[3]/div[3]/div/div/ul[2]/li[7]/text()")?.InnerText.Trim();
-                        var studio = htmlitem.DocumentNode.SelectSingleNode("/html/body/div[3]/div[3]/div/div/ul[2]/li[8]/ul/li/a")?.InnerText.Trim();
-                        var producer = htmlitem.DocumentNode.SelectSingleNode("/html/body/div[3]/div[3]/div/div/ul[2]/li[9]/a")?.InnerText.Trim();
-                        var type = htmlitem.DocumentNode.SelectSingleNode("//*[@id='animeType']/text()")?.InnerText.Trim();
-                        var series = htmlitem.DocumentNode.SelectSingleNode("/html/body/div[3]/div[3]/div/div/ul[2]/li[11]/text()")?.InnerText.Trim();
+                            var primarySourse = htmlitem.DocumentNode.SelectSingleNode("/html/body/div[3]/div[3]/div/div/ul[2]/li[7]/text()")?.InnerText.Trim();
+                            var studio = htmlitem.DocumentNode.SelectSingleNode("/html/body/div[3]/div[3]/div/div/ul[2]/li[8]/ul/li/a")?.InnerText.Trim();
+                            var producer = htmlitem.DocumentNode.SelectSingleNode("/html/body/div[3]/div[3]/div/div/ul[2]/li[9]/a")?.InnerText.Trim();
+                            var type = htmlitem.DocumentNode.SelectSingleNode("//*[@id='animeType']/text()")?.InnerText.Trim();
+                            var series = htmlitem.DocumentNode.SelectSingleNode("/html/body/div[3]/div[3]/div/div/ul[2]/li[11]/text()")?.InnerText.Trim();
 
-                        var transfer = htmlitem.DocumentNode.SelectSingleNode("/html/body/div[3]/div[3]/div/div/ul[2]/li[12]/ul")?.InnerText.Trim();
+                            var transfer = htmlitem.DocumentNode.SelectNodes("/html/body/div[3]/div[3]/div/div/ul[2]/li[12]/ul/li/text()");
 
-                        var voiceActing = htmlitem.DocumentNode.SelectSingleNode("/html/body/div[3]/div[3]/div/div/ul[2]/li[13]/ul")?.InnerText.Trim();
+                            var voiceActing = htmlitem.DocumentNode.SelectSingleNode("/html/body/div[3]/div[3]/div/div/ul[2]/li[13]/ul/li/text()");
 
-                        var description = htmlitem.DocumentNode.SelectSingleNode("//*[@id='content - desc - text']/p")?.InnerText.Trim();
+                            var description = htmlitem.DocumentNode.SelectSingleNode("//*[@id='content - desc - text']/p/text()")?.InnerText.Trim();
 
-                        var urlImage = htmlitem.DocumentNode.SelectSingleNode("/html/body/div[3]/div[3]/div/div/div[1]/div[1]/img")?.InnerText.Trim();
-                        var licende = htmlitem.DocumentNode.SelectSingleNode("//*[@id='video']/div")?.InnerText.Trim();
-                        #endregion
+                            var urlImage = htmlitem.DocumentNode.SelectSingleNode("/html/body/div[3]/div[3]/div/div/div[1]/div[1]/img")?.GetAttributeValue("src", "").Trim();
+                            var licende = htmlitem.DocumentNode.SelectSingleNode(".//div[@id='video']/div[@class='status-bg alert-bg']/text()")?.InnerText.Trim();
+                            #endregion
 
-                        //_entries.Add(new EntryModel
-                        //{
-                        //    Number = counter,
-                        //    Title = title,
-                        //    Rating = rating,
-                        //    Vote = vote,
-                        //    titleList = titleList,
-                        //    View = view,
-                        //    Status = status,
-                        //    Released = released,
-                        //    Season = season,
-                        //    ageRating = ageRating,
-                        //    Genre = genre,
-                        //    primarySource = primarySourse,
-                        //    Studio = studio,
-                        //    Producer = producer,
-                        //    Type = type,
-                        //    Series = series,
-                        //    Transfer = transfer,
-                        //    voiceActing = voiceActing,
-                        //    Description = description,
-                        //    urlImage = "https://yummyanime.club" + urlImage,
-                        //    License = licende
-                        //});
+                            //_entries.Add(new EntryModel
+                            //{
+                            //    Number = counter,
+                            //    Title = title,
+                            //    Rating = rating,
+                            //    Vote = vote,
+                            //    titleList = titleList,
+                            //    View = view,
+                            //    Status = status,
+                            //    Released = released,
+                            //    Season = season,
+                            //    ageRating = ageRating,
+                            //    Genre = genre,
+                            //    primarySource = primarySourse,
+                            //    Studio = studio,
+                            //    Producer = producer,
+                            //    Type = type,
+                            //    Series = series,
+                            //    Transfer = transfer,
+                            //    voiceActing = voiceActing,
+                            //    Description = description,
+                            //    urlImage = "https://yummyanime.club" + urlImage,
+                            //    License = licende
+                            //});
+                        }
                     }
                 }
                 catch (NullReferenceException exc)
